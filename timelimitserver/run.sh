c6fb11777b483c5e6258de6d4cbf7d5cf38443f7
jeroenlind <157898720+JeroenLind@users.noreply.github.com>
2026-01-16 15:09:12 +0100

    Add echo statement for addon version

--- timelimitserver/run.sh
+++ timelimitserver/run.sh
@@ -2,6 +2,7 @@
 # Lees versie uit /etc/hassio.json (altijd aanwezig in HA add-ons)
 ADDON_VERSION=$(jq -r '.version // empty' /etc/hassio.json 2>/dev/null)
 
+echo "TimeLimit add-on version: ${ADDON_VERSION:-unknown}"
 echo "TimeLimit test add-on container is starting..."
 
 counter=0
